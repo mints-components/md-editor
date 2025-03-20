fb36a20e483bdb0d7a07728a7470503ae2e95d38
mintsweet <0x1304570@gmail.com>
2025-03-20 20:07:12 +1300

    chore: update package information

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'my-component',
-      fileName: 'my-component',
+      name: 'md-editor',
+      fileName: 'md-editor',
     },
     rollupOptions: {
       external: [
